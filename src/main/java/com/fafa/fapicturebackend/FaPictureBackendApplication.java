dc527896ee45cb994c4cdb130ac6186c2a357b93
Fah1nCey <fafa197@163.com>
2025-12-07 15:56:36 +0800

    chore: 项目初始化

---
+++ src/main/java/com/fafa/fapicturebackend/FaPictureBackendApplication.java
@@ -0,0 +1,17 @@
+package com.fafa.fapicturebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.fafa.fapicturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class FaPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fafa/fapicturebackend/FaPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fafa.fapicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
